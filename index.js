b68b4fd270a174d17941ffa83853ac11a88519da
Tomek Niepewny <niepewny.t@hotmail.com>
2020-05-25 19:24:19 +0200

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-document.querySelectorAll('.drag-area').forEach(element => {
-     element.addEventListener('click', () => {
-          element.parentNode.classList.toggle('closed');
-     });
-});
+// document.querySelectorAll('.drag-area').forEach(element => {
+//      element.addEventListener('click', () => {
+//           element.parentNode.classList.toggle('closed');
+//      });
+// });
+
+const mouth = document.querySelector(".mouth")
+
+const leftEye = document.querySelector(".eye1")
+
+const rightEye = document.querySelector(".eye2")
+
+const switchBtn = document.querySelector(".switchBtn")
+
+const tongue = document.querySelector(".tongue")
+
+const allElements = [mouth, leftEye, rightEye, switchBtn, tongue]
+
+switchBtn.addEventListener("click", happyFace)
+
+function happyFace() {
+     allElements.forEach(element => {element.classList.toggle("happy")})
+}
